6cfcaed5a92c5ad8088650e94a97934c9ce0b463
Dominic <string.reverse@proton.me>
2024-09-01 20:49:06 -0400

    Implemented basic rating functionality
    Component renders the correct rating given a value

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 
             <div className="flex align-items-center justify-content-center container">
                 <div className="card pa-16">
-                    <Rating/>
+                    <Rating value={3.5}/>
                 </div>
             </div>
         </div>
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,39 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Rating.css';
 
-const Rating = (props) => {
-    const emptyIcon =  '/icons/stars/empty.svg';
-    const filledIcon =  '/icons/stars/filled.svg';
-    const halfFilledIcon =  '/icons/stars/half.svg';
+const MAX_RATING = 5;
+
+const FILL_TYPE = {
+    EMPTY: 0,
+    FILLED: 1,
+    HALF_FILLED: 0.5,
+}
+
+const Rating = ({value = 0, emptyIcon, halfFilledIcon, filledIcon, steps}) => {
+    emptyIcon =  '/icons/stars/empty.svg';
+    filledIcon =  '/icons/stars/filled.svg';
+    halfFilledIcon =  '/icons/stars/half.svg';
+
+    const [valArr, setValArr] = useState([]);
+
+    useEffect(() => {
+        setValArr(createRatingArray(value));
+    }, [value]);
+
+    function createRatingArray(rating) {
+        const temp = new Array(Math.floor(rating)).fill(1);
+        if (rating % 1 > 0) {
+            temp.push(0.5);
+        }
+        for (let i = 0; i < MAX_RATING - Math.ceil(rating); i++) {
+            temp.push(0);
+        }
+        return temp;
+    }
 
     // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
-    const isLessThanHalf = (event) => {
+    function isLessThanHalf(event){
         const {target} = event;
         const boundingClientRect = target.getBoundingClientRect();
         let mouseAt = event.clientX - boundingClientRect.left;
         mouseAt = Math.round(Math.abs(mouseAt));
         return mouseAt <= boundingClientRect.width / 2;
-    };
+    }
 
-    const renderSymbol = () => {
+    function renderSymbol(ratingVal, index) {
+        let iconSrc = undefined;
+        switch(ratingVal) {
+            case FILL_TYPE.FILLED:
+                iconSrc = filledIcon;
+                break
+            case FILL_TYPE.HALF_FILLED:
+                iconSrc = halfFilledIcon;
+                break;
+            case FILL_TYPE.EMPTY:
+                iconSrc = emptyIcon;
+                break;
+            default:
+                break;
+        }
         return (
-            <img src=""
-                 className="rating-image"
-                 data-testid="rating-icon"
-                 alt="Rate"/>
+          <img src={iconSrc}
+               className="rating-image"
+               data-testid="rating-icon"
+               alt="Rate"
+               key={index}
+          />
         )
     }
 
     return (
-        <div
-            tabIndex="0"
-            className="star-rating"
-            data-testid="star-rating-container"
-        >
-            {
-                renderSymbol()
-            }
-        </div>
+      <div
+        tabIndex="0"
+        className="star-rating"
+        data-testid="star-rating-container"
+      >
+          {
+              valArr.map((ratingVal, i) => {
+                  return renderSymbol(ratingVal, i);
+              })
+          }
+      </div>
     )
 };
 
